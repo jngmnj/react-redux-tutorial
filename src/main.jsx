784c0e416803e36fc1571f267ee44951e1135542
jngminj <exorbitance@naver.com>
2023-12-11 22:41:44 +0900

    [ADD] redux 만들기

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,15 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import rootReducer from './modules/index.js'
+import { legacy_createStore as createStore } from 'redux'
+import { Provider } from 'react-redux'
+import { composeWithDevTools } from 'redux-devtools-extension';
+
+const store = createStore(rootReducer,composeWithDevTools());
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
+  </Provider>
 )
